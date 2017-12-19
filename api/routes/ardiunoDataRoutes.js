acdfc2fba0bc1bd60501c9397bcfb8ef2b2812b6
LaYatiX <gpiwosz@gmail.com>
2017-12-19 03:22:45 +0100

    fixRouting

--- api/routes/ardiunoDataRoutes.js
+++ api/routes/ardiunoDataRoutes.js
@@ -3,12 +3,14 @@ module.exports = function(app) {
   var arduino = require('../controllers/arduinoController');
 
   // arduino Routes
-  app.route('/api/send')
-    .post(arduino.create_measurement);
+app.route('/api/send')
+.post(arduino.create_measurement);
 
-  app.route('/api/getall')
-    .get(arduino.read_all_measurements)
+app.route('/api/getall')
+.get(arduino.read_all_measurements)
 
-    app.route('/api/getlatest')
+app.route('/api/getlatest')
     .get(arduino.read_last_measurement)
+app.route('/')
+    .res.sendFile('../../public/index.html')
 };  
